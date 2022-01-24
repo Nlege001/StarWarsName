7adf879e9a464674f4dc719513e8dc054806d1e5
Naol Legesse <nlege001@plattsburgh.edu>
2022-01-24 15:40:33 -0500

    edited ui

--- app/src/main/java/com/example/starwarsname/MainActivity.java
+++ app/src/main/java/com/example/starwarsname/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,8 +29,18 @@ public class MainActivity extends AppCompatActivity {
         CityBorn = findViewById(R.id.City);
         StarName = findViewById(R.id.Name);
         ImageButton generate = findViewById(R.id.Generate);
+        ImageButton clear = findViewById(R.id.clear);
+
+        clear.setOnClickListener(v -> {
+            FirstName.getText().clear();
+            LastName.getText().clear();
+            MomsMiddleName.getText().clear();
+            CityBorn.getText().clear();
+            StarName.setText("Please provide Fields to get Star wars name");
+        });
 
         generate.setOnClickListener(v -> {
+
             calculateName();
         });
 
@@ -42,7 +53,21 @@ public class MainActivity extends AppCompatActivity {
         String momMiddle = MomsMiddleName.getText().toString();
         String born = CityBorn.getText().toString();
 
-        String starWarsName = "Welcome! " + last.substring(0,3) + first.substring(0,2) + " " + momMiddle.substring(0,2) + born.substring(0,2) + ", May the force be with you!";
-        StarName.setText(starWarsName);
+        if(first == ""){
+            Toast.makeText(getApplicationContext(), "Please provide First Name", Toast.LENGTH_SHORT).show();
+        }
+        else if(last == ""){
+            Toast.makeText(getApplicationContext(), "Please provide Last Name", Toast.LENGTH_SHORT).show();
+        }
+        else if(momMiddle == ""){
+            Toast.makeText(getApplicationContext(), "Please provide Mom's Maiden Name", Toast.LENGTH_SHORT).show();
+        }
+        else if(born == ""){
+            Toast.makeText(getApplicationContext(), "Please provide City of birth", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            String starWarsName = "Welcome! " + last.substring(0, 3) + first.substring(0, 2) + " " + momMiddle.substring(0, 2) + born.substring(0, 2) + ", May the force be with you!";
+            StarName.setText(starWarsName);
+        }
     }
 }
